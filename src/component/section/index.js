a15a7fd743adecf8cb1903ec8f05ba18ad637639
Haseeb-daar <abdul.haseeb@purevpn.com>
2022-03-30 21:57:49 +0500

    d

--- src/component/section/index.js
+++ src/component/section/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 import video from '../../assets/video.mp4'
 import icon from '../../assets/WHITE.gif'
+import logo from '../../assets/logo.png'
+
 import { FiVolume2, FiVolumeX } from "react-icons/fi";
 import { useState } from 'react';
 import Typed from 'react-typed';
@@ -8,23 +10,26 @@ import Typed from 'react-typed';
 
 function FirstSection() {
     const [volume, setVolume] = useState(false);
-    const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(true);
     function handleVolume(sound) {
         setVolume(!sound);
     }
     function handleVisibilty() {
-        setVisible(!visible);
+        setVisible(visible);
     }
 
 
     return (
         <div className="main_container">
             <div className='top_bar'>
+           
+                <div className='logo_container'>
+                    <img src={logo} alt='' />
+                </div>
                 <Typed
                     strings={['Coming Soon...']}
                     typeSpeed={60}
                     backSpeed={70}
-
                     loop
                 />
             </div>
@@ -51,16 +56,15 @@ function FirstSection() {
                         )}
                     </span>
                 ) : null}
-                <video playsInline loop autoPlay='true'muted={!volume}>
+                <video playsInline loop autoPlay='true' muted={!volume}>
                     <source src={video} type="video/mp4" />
                 </video>
             </div>
             <div className='bottom_bar'>
-                <div className='img_wrapper'>
+                <div className='img_wrapper' onClick={() => window.open('https://discord.link/PartyApesClub', '_blank')}>
                     <img src={icon} alt='' />
-
                 </div>
-                <span>DISCORD</span>
+                <span onClick={() => window.open('https://discord.link/PartyApesClub', '_blank')}>DISCORD</span>
             </div>
         </div>
     );
